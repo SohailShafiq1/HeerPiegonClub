c1d636438eef8310879bddbade03e7bbb588dcdc
Israr Ahmad <154470627+israrahmad831@users.noreply.github.com>
2025-03-15 23:52:02 +0500

    added event

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "./components/NavBar/Navbar";
-
+import Events from "./components/Events/Events";
 const home = () => {
   return (
     <div>
+      <Events />
       <div>Home</div>
     </div>
   );
