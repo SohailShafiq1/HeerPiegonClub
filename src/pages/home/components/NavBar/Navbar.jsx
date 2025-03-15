5cb9efaa55f28b06c3696465bd4242ac28d21ae7
Sohail Shafiq <sohail.shafiq002@gmail.com>
2025-03-15 23:16:54 +0500

    adding home page and pages folder

---
+++ src/pages/home/components/NavBar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import style from './Navbar.module.css'
+const s  = style
+import logo from '../../../../assets/logo.png'
+const Navbar = () => {
+  return (
+    <div className={s.container} >
+      <div className={s.logo}>
+        <img src={logo} alt="" />
+      </div>
+      <div className={s.navbar}>
+        <button>
+                Home
+        </button>
+        <button>
+                Weather
+        </button>
+        <button>
+                Tournaments
+        </button>
+        <button>
+                Contact Us
+        </button>
+      </div>
+      <div className={s.language}>
+        <button>
+            Urdu
+        </button>
+        <button>
+            English
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
