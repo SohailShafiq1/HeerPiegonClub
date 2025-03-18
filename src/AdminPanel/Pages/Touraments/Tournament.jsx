7539df23e3f3972f906683531f37fb03cb0804be
SohailShafiq1 <sohail.shafiq002@gmail.com>
2025-03-18 22:21:19 +0500

    Pushing table of tournaments

--- src/AdminPanel/Pages/Touraments/Tournament.jsx
+++ src/AdminPanel/Pages/Touraments/Tournament.jsx
@@ -1,20 +1,68 @@
-import { IoAddCircleOutline } from "react-icons/io5"; 
-import React from 'react'
-import style from './tournament.module.css'
-const s = style
+import { BiEdit } from "react-icons/bi";
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
+import { IoAddCircleOutline } from "react-icons/io5";
+import React from "react";
+import style from "./tournament.module.css";
+import { FaMinusCircle } from "react-icons/fa";
+const s = style;
 const Tournament = () => {
+  const data = [
+    { name: "Lahore" },
+    { name: "Faisalabad" },
+    { name: "Rawalpindi" },
+    { name: "Multan" },
+    { name: "Gujranwala" },
+    { name: "Sialkot" },
+    { name: "Bahawalpur" },
+    { name: "Sargodha" },
+    { name: "Sheikhupura" },
+    { name: "Jhelum" },
+    { name: "Amritsar" },
+    { name: "Ludhiana" },
+    { name: "Patiala" },
+    { name: "Jalandhar" },
+    { name: "Bathinda" },
+  ];
+
   return (
     <div className={s.container}>
       <div className={s.top}>
-        <h1>
-            Tournament
-        </h1>
-        
+        <h1>Tournament</h1>
+
         <IoAddCircleOutline className={s.add} />
-        
+      </div>
+      <div className={s.table}>
+        <table className={s.tournamenttable}>
+          <thead>
+            <tr>
+              <th>Count</th>
+              <th>Name</th>
+              <th>Add</th>
+              <th>Edit</th>
+              <th>Remove </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((tournament, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{tournament.name}</td>
+                <td>
+                  <AiOutlineUsergroupAdd className={s.addperson} />
+                </td>
+                <td>
+                  <BiEdit className={s.edit} />
+                </td>
+                <td>
+                  <FaMinusCircle className={s.remove} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tournament
+export default Tournament;
